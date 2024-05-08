e20829a42e8ef7e3de9757841a46e28318fb8209
Leo Walker <leowalker89@gmail.com>
2024-05-08 14:51:01 -0700

    changed shape of top with image

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide")
 
 st.title("Job Description Parser")
 
-col1, col2 = st.columns([1,6])
+col1, col2 = st.columns([2,6])
 
 with col1:
     st.image("images/jobDetailExtractor3part.png", use_column_width=True)
